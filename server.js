b9baebe997f0ca8620c599dbab45c02a2c00b88d
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2023-07-25 00:06:08 -0300

    feat: strip integration

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv'
+
+//Load variables 
+dotenv.config ();
+
+//Start Server
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+//Home Route
+app.get("/", (req, res) => {
+    res.sendFile("index.html", {root: "public"});
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000;");
+});
